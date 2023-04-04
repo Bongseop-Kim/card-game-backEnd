705d6a29e499d46e7d8b6d830476c1196cebda43
Bongseop-Kim <96857444+Bongseop-Kim@users.noreply.github.com>
2023-04-04 22:23:15 +0900

    nest 초기 세팅, interceptor, exception, logger

---
+++ src/main.ts
@@ -0,0 +1,31 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/exception/http-exception.filter';
+import * as expressBasicAuth from 'express-basic-auth';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  app.use(
+    ['/docs', '/docs-json'],
+    expressBasicAuth({
+      challenge: true,
+      users: {
+        [process.env.SWAGGER_USER]: process.env.SWAGGER_PASSWORD,
+      },
+    }),
+  );
+
+  app.enableCors({
+    origin: true,
+    //원하는 주소만 origin에 입력하여 접속을 허락
+    //cors 에러
+    credentials: true,
+  });
+
+  await app.listen(8000);
+}
+bootstrap();
